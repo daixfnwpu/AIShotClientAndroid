3b453ae434c41db5c853d5cbb7a744b0653561fe
daixfnwpu <daixfnwpu@gmail.com>
2024-08-31 16:04:45 +0800

    can not visit the web server

--- app/src/main/java/com/ai/aishotclientkotlin/util/Constans.kt
+++ app/src/main/java/com/ai/aishotclientkotlin/util/Constans.kt
@@ -3,15 +3,15 @@ package com.ai.aishotclientkotlin.util
 
 object Constants {
 
-    val TYPEONE : String = "http://localhost:8080/"
-    val TYPETWO : String = "Secret Key"
-    val LOGIN : String = "Page Url"
-    val REGISTER : String = "Page Url"
-    val LANGUAGES : String = "Page Url"
-    val WORDS : String = "Page Url"
+    const val TYPEONE : String = "http://192.168.3.116:8080/"
+    const val TYPETWO : String = "Secret Key"
+    const val LOGIN : String = TYPEONE + "login"
+    const val REGISTER : String = TYPEONE + "register"
+    const val LANGUAGES : String = TYPEONE + "languages"
+    const val WORDS : String = TYPEONE + "words"
 
-    val splashImagePath: String = TYPEONE + "Image Url"
-    val LoginImagePath: String = TYPEONE + "Image Url"
-    val LanguageImagePath: String = TYPEONE + "Image Url"
+    val splashImagePath: String = TYPEONE + "images/1.jpg"
+    val LoginImagePath: String = TYPEONE + "images/2.jpg"
+    val LanguageImagePath: String = TYPEONE + "images/3.jpg"
 
 }
